0bba2c865b0f19a723fd2ac0aee6aa25ad52c668
Sapna446 <58971333+Sapna446@users.noreply.github.com>
2021-05-06 10:56:00 +0530

    #DEVOPS-62 testing commit check

--- PlatformUI3/src/app/insights.module.ts
+++ PlatformUI3/src/app/insights.module.ts
@@ -12,7 +12,8 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
  * License for the specific language governing permissions and limitations under
  * the License.
- ******************************************************************************/
+ ******************************************************************************
+ testing/
 
 import { DomSanitizer, BrowserModule, SafeUrl } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
